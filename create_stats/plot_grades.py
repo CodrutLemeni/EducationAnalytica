9845beeddb1d0aad4a60276cb4df47c6713e1610
codrut <codrutlemeni@gmail.com>
2019-10-03 14:27:06 +0300

    plot works

--- create_stats/plot_grades.py
+++ create_stats/plot_grades.py
@@ -5,27 +5,84 @@ import statistics
 
 input_file = r"D:\Work\bac_stats\stats_bac\data\gender_results.txt"
 input_file = r"D:\Work\bac_stats\stats_bac\data\specializations\mate_info.txt"
-input_file = r"D:\Work\bac_stats\stats_bac\data\specializations\filologie.txt"
+# input_file = r"D:\Work\bac_stats\stats_bac\data\specializations\filologie.txt"
+
+def read_data(input_file):
+    fin = open(input_file)
+
+    grades = fin.read()
+    grades = grades.split('\n')
+
+    boys_grades = []
+    girls_grades = []
+    unknown_grades = []
+
+    for cr_grade in grades:
+        cr_grade = cr_grade.split(' ')
+        if cr_grade[0] == 'M':
+            boys_grades.append( float(cr_grade[-1]) )
+        elif cr_grade[0] == 'F':
+            girls_grades.append( float(cr_grade[-1]) )
+        else:
+            unknown_grades.append( float(cr_grade[-1]) )
+    return boys_grades, girls_grades, unknown_grades
+
+def create_histogram_array(grades):
+    histogram = [0]*5
+
+    # for [idx,cr_grade] in enumerate(grades):
+    #     histogram[idx] = int(cr_grade)
+    #     if histogram[idx] == 10:
+    #         histogram[idx] = 9
+    for [idx,cr_grade] in enumerate(grades):
+        if cr_grade >= 5.00 and cr_grade < 6.00:
+            histogram[0] = histogram[0] + 1 
+        elif cr_grade >= 6.00 and cr_grade < 7.00:
+            histogram[1] = histogram[1] + 1 
+        elif cr_grade >= 7.00 and cr_grade < 8.00:
+            histogram[2] = histogram[2] + 1 
+        elif cr_grade >= 8.00 and cr_grade < 9.00:
+            histogram[3] = histogram[3] + 1 
+        elif cr_grade >= 9.00 and cr_grade <= 10.00:
+            histogram[4] = histogram[4] + 1 
+    
+    return histogram
 
 
 
-fin = open(input_file)
+def make_histogram(boys_grades, girls_grades):
+    boys_histogram = create_histogram_array(boys_grades)
+    girls_histogram = create_histogram_array(girls_grades)
 
-grades = fin.read()
-grades = grades.split('\n')
+    boys_histogram = np.array(boys_histogram)/len(boys_grades)*100
+    girls_histogram = np.array(girls_histogram)/len(girls_grades)*100
 
-boys_grades = []
-girls_grades = []
-unknown_grades = []
+    boys_histogram = boys_histogram.astype(int)
+    girls_histogram = girls_histogram.astype(int)
 
-for crGrade in grades:
-    crGrade = crGrade.split(' ')
-    if crGrade[0] == 'M':
-        boys_grades.append( float(crGrade[-1]) )
-    elif crGrade[0] == 'F':
-        girls_grades.append( float(crGrade[-1]) )
-    else:
-        unknown_grades.append( float(crGrade[-1]) )
+    plot_boys=[]
+    plot_girls=[]
+    for (idx, val) in enumerate(boys_histogram):
+        for i in range(val):
+            plot_boys.append(idx+5.5)
+
+    for (idx, val) in enumerate(girls_histogram):
+        for i in range(val):
+            plot_girls.append(idx+5.5)
+
+    plt.hist( [plot_boys,plot_girls], color=['blue', 'orange'] )
+    plt.legend(['Baieti', 'Fete'])
+    plt.ylabel('Procentaj')
+    plt.xlabel('Note')
+    plt.xticks(range(5,11))
+    plt.yticks(range(0,40,5))
+    # plt.hist( [boys_grades,girls_grades], 5)
+
+    plt.show()
+
+if __name__ == "__main__":
+    boys_grades, girls_grades, unknown_grades = read_data(input_file)     
+    make_histogram( boys_grades, girls_grades)
 
 
 print('Girls mean: ', statistics.mean(girls_grades))
