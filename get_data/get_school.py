636005cb1db716d7d172bd333651667033215dbc
gothera <cosminn01rm@gmail.com>
2019-10-05 11:59:00 +0300

    commit get_schools

--- get_data/get_school.py
+++ get_data/get_school.py
@@ -4,16 +4,14 @@ import os
 import requests
 from bs4 import BeautifulSoup
 
-output_file = r"/home/cosmi/BAC/BAC_2019_statistics/data/2018/raw_county.txt"
+output_file = r"/home/cosmi/BAC/BAC_2019_statistics/data/raw_county.txt"
 
-def get_data_website(page_url, idx):
+def get_data_website(page_url):
 
-    # opener = urllib.request.FancyURLopener({})
-    # f = opener.open(page_url)
     page = requests.get(page_url)
     soup = BeautifulSoup(page.text, 'html.parser')
     f = soup.prettify()
-    txt = '(<a href="......[A-Z]{1,2}.index.html">.*\n)(.*)(\n.*)'
+    txt = '(<a href="......*.lista_unitati.index.html">.*\n)(.*)(\n.*)'
     x = re.findall(txt, f)
     f = open(output_file, "a")
     for s in x:
@@ -31,4 +29,4 @@ if __name__ == "__main__":
 
     for idx in range(1, 9668 ):
         new_url = base_url + str(idx) + '.html'
-        get_data_website(new_url, idx)
+        get_data_website(new_url)
