e6ac0b64c133601296dfcf8350130290d5d2daf0
gothera <cosminn01rm@gmail.com>
2019-10-13 13:48:03 +0300

    updated box-plots

--- classes/student.py
+++ classes/student.py
@@ -1,6 +1,6 @@
 import csv
 import sys
-sys.path.append(r'/home/sebastian/Dropbox/Facultate/BacStats/BAC_2019_statistics')
+sys.path.append(r'../')
 from classes.highschool import *
 class Student:
     def __init__(self, gender, specialisation, medium, highschool, class_name, passed,
@@ -122,7 +122,7 @@ def filter_by_medium(all_students, medium):
     '''
     selected_students = []
     for current_student in all_students:
-        if current_student.medium >= medium:
+        if current_student.medium == medium:
             selected_students.append(current_student)
     return selected_students    
 
@@ -152,8 +152,8 @@ def get_gender_distribution(all_students):
 
 if __name__ == "__main__":
     
-    results_csv_file = r'/home/sebastian/Dropbox/Facultate/BacStats/good_bac_2019.csv'
-    schools_csv_file = r'/home/sebastian/Dropbox/Facultate/BacStats/unitati_scolare_2019.csv'
+    results_csv_file = r'../data/2019/good_bac_2019.csv'
+    schools_csv_file = r'../data/2019/unitati_scolare_2019.csv'
 
     all_students = initialiaze_students(results_csv_file, schools_csv_file)
     mate_info_students = filter_by_specialisation(all_students, 'matematica-informatica' )
