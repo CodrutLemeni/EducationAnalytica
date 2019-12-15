1d8db3e39fedb330f13060545497edfe88f81506
Sebastian <samoilescusebastian@gmail.com>
2019-12-15 17:56:15 +0200

    all_pizzacharts function created

--- create_stats/make_pizzachart.py
+++ create_stats/make_pizzachart.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 def make_pizzachart(numbers, labels, title, export_path):
-    plt.pie(numbers, autopct='%1.1f%%', shadow = True, startangle = 140, radius = 1.9)
-    plt.legend(labels)
+    plt.pie(numbers, autopct='%1.1f%%', startangle = 180, radius = 1.4)
     plt.axis('equal')
     plt.title(title)
+    plt.legend(labels)
+    figure=plt.gcf()
+    figure.set_size_inches(18.6,9.8)
     plt.savefig(export_path)
-
-    # plt.show()
+    plt.close()
