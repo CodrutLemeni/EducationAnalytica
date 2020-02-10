80514970b191a724fef27daa5216ed97816f852c
CodrutLemeni <codrutlemeni@gmail.com>
2020-02-10 19:06:33 +0200

    All line graphs (#40)
    * create_all_line_graphs function created
    
    * create_all_line_graphs function created
    
    Co-authored-by: samoilescusebastian <32140412+samoilescusebastian@users.noreply.github.com>

--- read_data/read_students.py
+++ read_data/read_students.py
@@ -1,14 +1,24 @@
 import sys
+import os
 from pathlib import Path
-sys.path.append(r'..')
+
+dirpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append( os.path.dirname(os.path.dirname(os.path.abspath(__file__))) ) 
+
 from classes.student import initialize_students
+
+
+results_base_path = os.path.join(dirpath,"data")
+results_base_path = os.path.join(results_base_path,"good_bac_year.csv")
+
 
 def read_results(years):
     results = {}
-    results_base_path = r"../data/year/good_bac_year.csv"
-    schools_csv_file = Path(r"../data/2019/unitati_scolare_2019.csv")
+    # results_base_path = r"../data/good_bac_year.csv"
+    # schools_csv_file = Path(r"../data/unitati_scolare_2019.csv")
     for year in years:
         current_results_path = results_base_path.replace("year", year)
         results_csv_file = Path(current_results_path)
-        results[year] = initialize_students(results_csv_file, schools_csv_file)
+        # results[year] = initialize_students(results_csv_file, schools_csv_file)
+        results[year] = initialize_students(results_csv_file)
     return results
