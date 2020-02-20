84fe2598e2838ac2dda2f775c52c4d726278d93b
gothera <cosminn01rm@gmail.com>
2020-02-20 20:15:39 +0200

    added linegraphs to display comparison between each 3 specialisations

--- create_stats/make_linegraph.py
+++ create_stats/make_linegraph.py
@@ -1,16 +1,20 @@
 import matplotlib.pyplot as plt
 
-def make_linegraph(grades, years, title, export_path, x_label = 'Anul', y_label = 'Nota',
-                   first_category = "Baieti", second_category = "Fete"):
+marker_colors = ['blue', 'red', 'black']
+line_colors = ['skyblue', 'coral', 'gray']
+def make_linegraph(grades, years, title, export_path, x_label = 'Anul', y_label = 'Nota', limits = None, categories = None):
     years_label = []
     for year in years:
         years_label.append(int(year))
-    plt.plot(years_label,grades[0],marker='o', markerfacecolor='blue', markersize=8, color='skyblue', linewidth=2)
-    plt.plot(years_label,grades[1],marker='o', markerfacecolor='red', markersize=8, color='coral', linewidth=2)
+    for i, means in enumerate(grades):
+        plt.plot(years_label,means,marker='o', markerfacecolor=marker_colors[i], markersize=8, color=line_colors[i], linewidth=2)
     axes = plt.gca()
-    axes.set_ylim([5, 9])
+    if limits is None:
+        axes.set_ylim([5, 9])
+    else:
+        axes.set_ylim(limits)
     axes.set_xlim([2014,2020])
-    plt.legend([first_category, second_category], loc='upper left')
+    plt.legend(categories, loc='upper left')
     plt.title(title)
     # plt.show()
     plt.savefig(export_path)
