1a2ecc44f737f8cd24a420f9a91e93305af8e3cb
Bogdan Circeanu <circeanu_bogdan@yahoo.com>
2020-02-24 15:57:19 +0200

    Remade make_barchart_percentage function Showing numbers along percents Made make_barchart_value function Made student filter by passing

--- create_stats/make_barchart.py
+++ create_stats/make_barchart.py
@@ -1,13 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt 
 
-def make_barchart_percentage(all_categories, categories_names, title, current_export_path, colors):
-    all_categories_no = 0
-    for category in all_categories:
-        all_categories_no += len(category)
+def make_barchart_percentage(all_categories, all_categories_good, categories_names, title, current_export_path, colors):
+
     percentage = []
-    for category in all_categories:
-        percentage.append(float(len(category)) / all_categories_no * 100)
+    for i in range(len(all_categories)):
+        percentage.append(float(len(all_categories_good[i])) / float(len(all_categories[i])) * 100)
+    
     
     plt.rcdefaults()
     fig,ax = plt.subplots()
@@ -21,7 +20,7 @@ def make_barchart_percentage(all_categories, categories_names, title, current_ex
         # converting width to int type
         width = int(rect.get_width())
 
-        value = percentage[i]
+        value = str(round(percentage[i], 2)) + '%'
         # Shift the text to the left side of the right edge
         xloc = -5
         # White on magenta
@@ -30,7 +29,7 @@ def make_barchart_percentage(all_categories, categories_names, title, current_ex
 
         # Center the text vertically in the bar
         yloc = rect.get_y() + rect.get_height() / 2
-        label = ax.annotate(str(round(value, 2)) + '%', xy=(width, yloc), xytext=(xloc, 0),
+        label = ax.annotate(str(len(all_categories_good[i])) + " - " + value, xy=(width, yloc), xytext=(xloc, 0),
                             textcoords="offset points",
                             ha=align, va='center',
                             color=clr, weight='bold', clip_on=True)
@@ -39,6 +38,46 @@ def make_barchart_percentage(all_categories, categories_names, title, current_ex
     ax.set_yticks(y_pos)
     ax.set_yticklabels(categories_names)
     ax.set_xlabel('Procentaj')
+    
+
+    ax.set_title(title)
+    #ax.annotate
+
+    plt.savefig(current_export_path)
+
+def make_barchart_values(all_categories, categories_names, title, current_export_path, colors):
+    
+    
+    plt.rcdefaults()
+    fig,ax = plt.subplots()
+    y_pos = np.arange(len(all_categories))
+
+    rects = ax.barh(y_pos, all_categories, align='center', color=colors)
+    i = 0
+    for rect in rects:
+        # Rectangle widths are already integer-valued but are floating
+        # type, so it helps to remove the trailing decimal point and 0 by
+        # converting width to int type
+        width = int(rect.get_width())
+
+        value = all_categories[i]
+        # Shift the text to the left side of the right edge
+        xloc = -5
+        # White on magenta
+        clr = '#010a43'
+        align = 'right'
+
+        # Center the text vertically in the bar
+        yloc = rect.get_y() + rect.get_height() / 2
+        label = ax.annotate(str(round(value, 2)), xy=(width, yloc), xytext=(xloc, 0),
+                            textcoords="offset points",
+                            ha=align, va='center',
+                            color=clr, weight='bold', clip_on=True)
+        i = i + 1
+
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(categories_names)
+    ax.set_xlabel('Nota')
     
 
     ax.set_title(title)
