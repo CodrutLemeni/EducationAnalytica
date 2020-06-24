a6372d70c336e6a4654d2ae3e45c60546e3e5f71
Bogdan Circeanu <circeanu_bogdan@yahoo.com>
2020-06-24 13:50:02 +0300

    Updated create_barchart_js and make_barchart_js

--- json/make_barchart_js.py
+++ json/make_barchart_js.py
@@ -2,32 +2,37 @@ import numpy as np
 import matplotlib.pyplot as plt
 import json
 
-def make_barchart_percentage_js(all_categories, all_categories_good, categories_names, title, current_export_path):
-
-    percentage = []
-    for i in range(len(all_categories)):
-        percentage.append(float(len(all_categories_good[i])) / float(len(all_categories[i])) * 100)
+def make_barchart_percentage_js(all_categories, all_categories_good, categories_general_names, categories_particular_names, title, current_export_path):
 
     meta = {}
     dataDict = {}
-    dataDict["series"] = []
+    dataDict["series_list"] = []
     
     
-    meta["type"] = "HORIZONTAL_BAR_CHART"
+    meta["type"] = "HORIZONTAL_BAR_CHART_GROUPED"
     meta["title"] = title
     meta["xAxisName"] = "Procentaj"
     dataDict["meta"] = meta
-    for i in range(len(all_categories)):
-        label = {}
-        crtElem = {}
-        crtElem["extra"] = []
+    
+    for i in range(len(categories_general_names)):
+        crt_series_with_name = {}
+        crt_series = []
+        crt_series_with_name["name"] = categories_general_names[i]
+        crt_list = all_categories[i]
+        crt_list_passed = all_categories_good[i]
+        for j in range(len(all_categories[i])):
+            label = {}
+            crt_elem = {}
+            crt_elem["extra"] = []
 
-        crtElem["key"] = categories_names[i]
-        crtElem["value"] = percentage[i]
-        label["label"] = "Nr. elevi promovati"
-        label["value"] = len(all_categories_good[i])
-        crtElem["extra"].append(label)
-        dataDict["series"].append(crtElem)
+            crt_elem["key"] = categories_particular_names[j]
+            crt_elem["value"] = crt_list_passed[j] / crt_list[j] * 100
+            label["label"] = "Nr. elevi promovati"
+            label["value"] = crt_list[j]
+            crt_elem["extra"].append(label)
+            crt_series.append(crt_elem)
+        crt_series_with_name["series"] = crt_series
+        dataDict["series_list"].append(crt_series_with_name)
 
     f = open(current_export_path + ".txt", "w+")
     jsonDict = json.dumps(dataDict)
@@ -73,23 +78,70 @@ def make_barchart_percentage_subject3_js(all_students, all_grades, categories_na
     jsonDict = json.dumps(dataDict)
     f.write(jsonDict)
 
-def make_barchart_values_js(all_categories, categories_names, title, current_export_path):
+def make_barchart_values_js(all_categories, categories_general_names, categories_particular_names, title, current_export_path):
+
     meta = {}
     dataDict = {}
-    dataDict["series"] = []
+    dataDict["series_list"] = []
     
     
-    meta["type"] = "HORIZONTAL_BAR_CHART"
+    meta["type"] = "HORIZONTAL_BAR_CHART_GROUPED"
     meta["title"] = title
     meta["xAxisName"] = "Nota"
     dataDict["meta"] = meta
-    for i in range(len(all_categories)):
-        crtElem = {}
-        crtElem["extra"] = []
+    
+    for i in range(len(categories_general_names)):
+        crt_series_with_name = {}
+        crt_series = []
+        crt_series_with_name["name"] = categories_general_names[i]
+        crt_list = all_categories[i]
+        for j in range(len(all_categories[i])):
+            label = {}
+            crt_elem = {}
+            crt_elem["extra"] = []
 
-        crtElem["key"] = categories_names[i]
-        crtElem["value"] = all_categories[i]
-        dataDict["series"].append(crtElem)
+            crt_elem["key"] = categories_particular_names[j]
+            crt_elem["value"] = crt_list[j]
+
+            crt_series.append(crt_elem)
+        crt_series_with_name["series"] = crt_series
+        dataDict["series_list"].append(crt_series_with_name)
+
+    f = open(current_export_path + ".txt", "w+")
+    jsonDict = json.dumps(dataDict)
+    f.write(jsonDict)
+
+
+def make_stacked_barchart_js(all_categories, vertical_names, horizontal_names, title, current_export_path):
+    meta = {}
+    dataDict = {}
+    dataDict["series_list"] = []
+    
+    
+    meta["type"] = "HORIZONTAL_BAR_CHART_STACKED"
+    meta["title"] = title
+    meta["xAxisName"] = "Procentaj"
+    dataDict["meta"] = meta
+    
+    for i in range(len(horizontal_names)):
+        crt_series_with_name = {}
+        crt_series = []
+        crt_series_with_name["name"] = horizontal_names[i]
+        
+        for j in range(len(all_categories)):
+            crt_list = all_categories[j]
+            label = {}
+            crt_elem = {}
+            crt_elem["extra"] = []
+
+            crt_elem["key"] = vertical_names[j]
+            crt_elem["value"] = crt_list[i] / sum(crt_list) * 100
+            label["label"] = "Numar elevi"
+            label["value"] = crt_list[i]
+            crt_elem["extra"].append(label)
+            crt_series.append(crt_elem)
+        crt_series_with_name["series"] = crt_series
+        dataDict["series_list"].append(crt_series_with_name)
 
     f = open(current_export_path + ".txt", "w+")
     jsonDict = json.dumps(dataDict)
