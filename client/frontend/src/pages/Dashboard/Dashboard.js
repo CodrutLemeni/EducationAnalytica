dc9159e1fdcf0d683b6c6b7e9f622e78a95a8de2
paulcbn <ciobanupaulgavril@gmail.com>
2020-06-25 22:04:01 +0300

    Add some demo layouting.

--- client/frontend/src/pages/Dashboard/Dashboard.js
+++ client/frontend/src/pages/Dashboard/Dashboard.js
@@ -15,11 +15,11 @@ const Dashboard = ({ loadCharts, gradeDistExample, averageGradeDistExample, coun
   useEffect(() => loadCharts(), [ loadCharts ]);
 
   return <Box>
-    <Chart height={ 500 } chartData={ keysToCamel({ data: jsonExampleGrouped, loading: false }) }/>
-    <Chart height={ 500 } chartData={ keysToCamel({ data: jsonExampleStacked, loading: false }) }/>
+    {/**/ }
+    {/**/ }
     <Grid container>
       <Grid item xs={ 12 }>
-        <TitleBox title={ 'Titlu mare si tare' }>
+        <TitleBox title={ 'Titlu principal' }>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit aliquet ultricies. Orci varius
             natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis faucibus purus ac justo
@@ -34,8 +34,8 @@ const Dashboard = ({ loadCharts, gradeDistExample, averageGradeDistExample, coun
       <Grid item xs={ 12 }>
         <Chart height={ 500 } chartData={ gradeDistExample }/>
       </Grid>
-      <Grid item xs={ 12 } md={ 12 }>
-        <Chart height={ 500 } chartData={ averageGradeDistExample }/>
+      <Grid item xs={ 12 } md={ 6 }>
+        <Chart height={ 500 } chartData={ keysToCamel({ data: jsonExampleGrouped, loading: false }) }/>
       </Grid>
       <Grid item xs={ 12 } md={ 6 }>
         <ArticleBox title={ 'Titlu articol' }>
@@ -61,7 +61,7 @@ const Dashboard = ({ loadCharts, gradeDistExample, averageGradeDistExample, coun
       </Grid>
 
 
-      <Grid item xs={ 12 } md={ 6 }>
+      <Grid item xs={ 12 } md={ 4 }>
         <ArticleBox title={ 'Titlu articol' }>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit aliquet ultricies. Orci varius
@@ -71,16 +71,29 @@ const Dashboard = ({ loadCharts, gradeDistExample, averageGradeDistExample, coun
             consequat elit magna posuere nisl. Nam scelerisque dui a luctus faucibus. Curabitur vestibulum orci vitae
             imperdiet commodo.
           </p>
-          <p>
-            Vestibulum posuere erat quam, eget iaculis libero porta ut. Pellentesque habitant morbi tristique senectus
-            et netus et malesuada fames ac turpis egestas. Sed quis dui eu velit egestas blandit vel a nunc. Lorem ipsum
-            dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque sodales lorem eget venenatis.
-          </p>
         </ArticleBox>
       </Grid>
-      <Grid item xs={ 12 } md={ 6 }>
+      <Grid item xs={ 12 } md={ 8 }>
         <Chart height={ 500 } chartData={ countyGradeDistExample }/>
       </Grid>
+    </Grid>
+
+
+    <Grid item xs={ 12 }>
+      <ArticleBox title={ 'Titlu articol' }>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed suscipit aliquet ultricies. Orci varius
+          natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Duis faucibus purus ac justo
+          dictum, quis finibus purus dignissim. Morbi venenatis non elit eget tincidunt. In quis dapibus orci, sit
+          amet pretium turpis. Curabitur malesuada, magna vitae mattis pellentesque, ligula arcu sodales odio, quis
+          consequat elit magna posuere nisl. Nam scelerisque dui a luctus faucibus. Curabitur vestibulum orci vitae
+          imperdiet commodo.
+        </p>
+      </ArticleBox>
+    </Grid>
+
+    <Grid item xs={ 12 }>
+      <Chart height={ 500 } chartData={ keysToCamel({ data: jsonExampleStacked, loading: false }) }/>
     </Grid>
   </Box>;
 };
