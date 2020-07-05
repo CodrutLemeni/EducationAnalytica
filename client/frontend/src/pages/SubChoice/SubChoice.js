7ef22e3cbcd708f94f6d5688bcdc0f3e9dd11715
paulcbn <ciobanupaulgavril@gmail.com>
2020-07-05 15:01:43 +0300

    Fix line graph widget "Filologie" not loading problem.
    Fixed issue #58.

--- client/frontend/src/pages/SubChoice/SubChoice.js
+++ client/frontend/src/pages/SubChoice/SubChoice.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 
-import { NotFound } from "../";
+import { NotFound } from '../';
 
-import { Chart } from "../../components/charts/Chart";
-import { TitleBox } from "../../components/TitleBox";
-import { Box, Grid } from "@material-ui/core";
+import { Chart } from '../../components/charts/Chart';
+import { TitleBox } from '../../components/TitleBox';
+import { Box, Grid } from '@material-ui/core';
 
-import { connect } from "react-redux";
-import { loadSubjectChoiceForSubject } from "../../lib/redux/actions/subjectChoice";
+import { connect } from 'react-redux';
+import { loadSubjectChoiceForSubject } from '../../lib/redux/actions/subjectChoice';
+
 /** Params may be:
  *  filologie, mate-info, resurse-naturale, profil-tehnic, stiinte ale naturii
  */
@@ -15,55 +16,55 @@ import { loadSubjectChoiceForSubject } from "../../lib/redux/actions/subjectChoi
 const subjects = [
   {
     id: 0,
-    label: "Filologie",
-    code: "filologie",
+    label: 'Filologie',
+    code: 'filologie',
     options:
-      "Geografie, Logică şi argumentare, Psihologie, Economie, Sociologie, Filosofie",
+      'Geografie, Logică şi argumentare, Psihologie, Economie, Sociologie, Filosofie',
   },
   {
     id: 1,
-    label: "Matematica Informatica",
-    code: "mate-info",
-    options: "Fizică, Chimie, Biologie, Informatică",
+    label: 'Matematica Informatica',
+    code: 'mate-info',
+    options: 'Fizică, Chimie, Biologie, Informatică',
   },
   {
     id: 2,
-    label: "Stiinte ale Naturii",
-    code: "stiinte-ale-naturii",
-    options: "Fizică, Chimie, Biologie, Informatică",
+    label: 'Stiinte ale Naturii',
+    code: 'stiinte-ale-naturii',
+    options: 'Fizică, Chimie, Biologie, Informatică',
   },
   {
     id: 3,
-    label: "Profil Tehnic",
-    code: "profil-tehnic",
-    options: "Fizică, Chimie, Biologie",
+    label: 'Profil Tehnic',
+    code: 'profil-tehnic',
+    options: 'Fizică, Chimie, Biologie',
   },
   {
     id: 4,
-    label: "Profil Resurse naturale şi protecţia mediului",
-    code: "profil-resurse-naturale",
-    options: "Fizică, Chimie, Biologie",
+    label: 'Profil Resurse naturale şi protecţia mediului',
+    code: 'profil-resurse-naturale',
+    options: 'Fizică, Chimie, Biologie',
   },
 ];
 
 const SubChoice = ({ match, subjectChoiceYears, loadCharts }) => {
   const subject = subjects.find((sub) => sub.code === match.params.subject);
-  useEffect(() => loadCharts(subject.code), [loadCharts]);
+  useEffect(() => loadCharts(subject.code), [ loadCharts, subject.code ]);
 
-  if (subject === undefined) return <NotFound />;
+  if (subject === undefined) return <NotFound/>;
   const title = subject.label;
 
   return (
     <Box>
-      <TitleBox title={title}>
+      <TitleBox title={ title }>
         Procentajul de alegere a fiecarei materii pentru subiectul III.
       </TitleBox>
-      <Grid container spacing={5}>
-        {Object.entries(subjectChoiceYears).map(([key, value], index) => (
-          <Grid item xs={12} md={6} key={index}>
-            <Chart height={500} chartData={value} />
+      <Grid container spacing={ 5 }>
+        { Object.entries(subjectChoiceYears).map(([ key, value ], index) => (
+          <Grid item xs={ 12 } md={ 6 } key={ index }>
+            <Chart height={ 500 } chartData={ value }/>
           </Grid>
-        ))}
+        )) }
       </Grid>
     </Box>
   );
