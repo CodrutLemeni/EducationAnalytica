93f5c36647d1f5cde55b5476d8b1203c72d85e2f
Marius Cristian Osiac <marius.osiac@student.manchester.ac.uk>
2020-07-15 00:08:27 +0300

    Fixed main page grade distribution

--- client/frontend/src/lib/redux/actions/globalStatistics.js
+++ client/frontend/src/lib/redux/actions/globalStatistics.js
@@ -1,46 +1,63 @@
-import API, { keysToCamel } from '../../api';
+import API, { keysToCamel } from "../../api";
 
 export const loadGlobalGradeDistribution = () => {
   return (dispatch, getState) => {
-    dispatch({ type: 'GLOBAL_GRADE_DISTRIBUTION_LOADING' });
-    API.get(`/api/grade-distributions/2019`)
-      .then(({ status, data }) => {
+    dispatch({ type: "GLOBAL_GRADE_DISTRIBUTION_LOADING" });
+    API.get(`/api/grade-distributions/general/2019`).then(
+      ({ status, data }) => {
         if (status === 200) {
-          dispatch({ type: 'GLOBAL_GRADE_DISTRIBUTION_LOADED', data: keysToCamel(data) });
+          dispatch({
+            type: "GLOBAL_GRADE_DISTRIBUTION_LOADED",
+            data: keysToCamel(data),
+          });
         } else if (status >= 400 && status < 500) {
-          dispatch({ type: 'GLOBAL_GRADE_DISTRIBUTION_ERROR', data: keysToCamel(data) });
+          dispatch({
+            type: "GLOBAL_GRADE_DISTRIBUTION_ERROR",
+            data: keysToCamel(data),
+          });
         }
-      });
+      }
+    );
   };
 };
 
 export const loadGlobalAverageGradePerProfileDistribution = () => {
   return (dispatch, getState) => {
-    dispatch({ type: 'GLOBAL_AVERAGE_GRADE_PER_PROFILE_DISTRIBUTION_LOADING' });
-    API.get(`/api/horizontal-bar-charts/fete-baieti-mate-info-filo-si-stiinte/2019`)
-      .then(({ status, data }) => {
-        if (status === 200) {
-          dispatch({ type: 'GLOBAL_AVERAGE_GRADE_PER_PROFILE_DISTRIBUTION_LOADED', data: keysToCamel(data) });
-        } else if (status >= 400 && status < 500) {
-          dispatch({ type: 'GLOBAL_AVERAGE_GRADE_PER_PROFILE_DISTRIBUTION_ERROR', data: keysToCamel(data) });
-        }
-      });
-
+    dispatch({ type: "GLOBAL_AVERAGE_GRADE_PER_PROFILE_DISTRIBUTION_LOADING" });
+    API.get(
+      `/api/horizontal-bar-charts/fete-baieti-mate-info-filo-si-stiinte/2019`
+    ).then(({ status, data }) => {
+      if (status === 200) {
+        dispatch({
+          type: "GLOBAL_AVERAGE_GRADE_PER_PROFILE_DISTRIBUTION_LOADED",
+          data: keysToCamel(data),
+        });
+      } else if (status >= 400 && status < 500) {
+        dispatch({
+          type: "GLOBAL_AVERAGE_GRADE_PER_PROFILE_DISTRIBUTION_ERROR",
+          data: keysToCamel(data),
+        });
+      }
+    });
   };
 };
 
 export const loadGlobalAverageGradePerCountyDistribution = () => {
   return (dispatch, getState) => {
-    dispatch({ type: 'GLOBAL_AVERAGE_GRADE_PER_COUNTY_DISTRIBUTION_LOADING' });
-    API.get(`/api/map-charts/2019`)
-      .then(({ status, data }) => {
-        if (status === 200) {
-          dispatch({ type: 'GLOBAL_AVERAGE_GRADE_PER_COUNTY_DISTRIBUTION_LOADED', data: keysToCamel(data) });
-        } else if (status >= 400 && status < 500) {
-          dispatch({ type: 'GLOBAL_AVERAGE_GRADE_PER_COUNTY_DISTRIBUTION_ERROR', data: keysToCamel(data) });
-        }
-      });
-
+    dispatch({ type: "GLOBAL_AVERAGE_GRADE_PER_COUNTY_DISTRIBUTION_LOADING" });
+    API.get(`/api/map-charts/2019`).then(({ status, data }) => {
+      if (status === 200) {
+        dispatch({
+          type: "GLOBAL_AVERAGE_GRADE_PER_COUNTY_DISTRIBUTION_LOADED",
+          data: keysToCamel(data),
+        });
+      } else if (status >= 400 && status < 500) {
+        dispatch({
+          type: "GLOBAL_AVERAGE_GRADE_PER_COUNTY_DISTRIBUTION_ERROR",
+          data: keysToCamel(data),
+        });
+      }
+    });
   };
 };
 
