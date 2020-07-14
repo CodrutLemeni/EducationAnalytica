7798df41ba1be9089f35853a2d2a2d9949dbd411
Marius Cristian Osiac <marius.osiac@student.manchester.ac.uk>
2020-07-15 00:19:10 +0300

    Fix static file serving bug

--- client/server.js
+++ client/server.js
@@ -30,7 +30,7 @@ db.initDb({ config }, ({ config, db }) => {
   app.use("/api", api.getRouter({ config, db }));
 
   /** React build static files */
-  app.use("/*", express.static("frontend/build"));
+  app.use(express.static("frontend/build"));
 
   app.listen(process.env.PORT || config.port, () => {
     console.log(`Server listening on port ${process.env.PORT || config.port}`);
