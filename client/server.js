9e5a3171e79899332f888733f414a299e94b0ae2
Marius Cristian Osiac <marius.osiac@student.manchester.ac.uk>
2020-07-15 00:06:04 +0300

    Fixed routing of static file 404s how handled by react

--- client/server.js
+++ client/server.js
@@ -30,19 +30,7 @@ db.initDb({ config }, ({ config, db }) => {
   app.use("/api", api.getRouter({ config, db }));
 
   /** React build static files */
-  app.use("/", express.static("frontend/build"));
-
-  /** 404 Route, allways keep last */
-  app.use("*", (req, res, next) => {
-    res.status(404).json({
-      error: {
-        code: 404,
-        msg: "Content not found",
-        description:
-          "We don't know what you are looking for, but make sure the URL is correct",
-      },
-    });
-  });
+  app.use("/*", express.static("frontend/build"));
 
   app.listen(process.env.PORT || config.port, () => {
     console.log(`Server listening on port ${process.env.PORT || config.port}`);
